a73bc97274ec04550a9a9dcba48af72093dc778d
Dritriy Golyshev <4dimariK@gmail.com>
2021-12-02 21:56:01 +0300

    завершено усложненное 6е задание

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+// УСЛОЖНЕННОЕ ЗАДАНИЕ
+// Загадывание случайного числа от 1 до 100
+
+const isNumber = num => !isNaN(parseFloat(num)) && isFinite(num) && !num.includes(' ');
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+function guessTheNumber(attemptQuantity) {
+    return function attempt(attemptCount = attemptQuantity, number = random(1, 100)) {
+        console.log(number); //Подсказка
+        if (attemptCount === 0) {
+            if (confirm('Попытки закончились, хотите сыграть еще?')) {
+                attempt(attemptQuantity);
+            } else return;
+        }
+        let answer = prompt('Угадай число от 1 до 100');
+
+        if (answer === null) {
+            alert('Игра окончена!');
+            return;
+        } else if (isNumber(answer) && +answer > number) {
+            attemptCount--;
+            if (attemptCount !== 0) {
+                alert(`Загаданное число меньше, осталось попыток ${attemptCount}`);
+                attempt(attemptCount, number);
+            } else {
+                if (confirm('Загаданное число меньше.\nПопытки закончились, хотите сыграть еще?')) {
+                    attempt(attemptQuantity, number); //если число не угадано, то оно остается прежним
+                } else return;
+            }
+        } else if (isNumber(answer) && +answer < number) {
+            attemptCount--;
+            if (attemptCount !== 0) {
+                alert(`Загаданное число больше, осталось попыток ${attemptCount}`);
+                attempt(attemptCount, number);
+            } else {
+                if (confirm('Загаданное число меньше.\nПопытки закончились, хотите сыграть еще?')) {
+                    attempt(attemptQuantity, number); //если число не угадано, то оно остается прежним
+                } else return;
+            }
+
+        } else if (isNumber(answer) && +answer === number) {
+            if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
+                attempt(attemptQuantity);
+            } else return;
+        } else {
+            alert('Введи число!');
+            attempt(attemptCount, number);
+        }
+    }
+
+
+
+}
+
+guessTheNumber(3)();
